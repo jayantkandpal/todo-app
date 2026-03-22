b91a7003ae9517e7b678ada315bd44ee289ab4db
jayantkandpal <jayant.kandpal0819@gmail.com>
2026-03-22 11:55:14 +0530

    Implement todo complete and exit app button

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ input_box = sg.InputText(tooltip="Enter todo", key="todo")
 
 add_button = sg.Button(tooltip="Click to add todo", button_text="Add")
 complete_button = sg.Button(tooltip="Click to mark todo as complete", button_text="Complete")
-show_button = sg.Button(tooltip="Click to view all todos", button_text="Show")
+exit_button = sg.Button(tooltip="Click to exit todos app", button_text="Exit")
 
 list_box = sg.Listbox(values=todoFunctions.get_todos(), enable_events=True,
                       key="todos", size=[45,10])
@@ -16,7 +16,8 @@ window = sg.Window('TODO app',
                    layout=[
                        [label],
                        [input_box, add_button],
-                       [list_box,edit_button]
+                       [list_box,[edit_button,complete_button]],
+                       [exit_button]
                    ],
                    font=("Helvetica", 16))
 while True:
@@ -40,6 +41,15 @@ while True:
             todos[index] = new_todo
             todoFunctions.write_todos(todos)
             window["todos"].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = todoFunctions.get_todos()
+            todos.remove(todo_to_complete)
+            todoFunctions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case 'todos':
             window["todo"].update(value=values['todos'][0].strip())
         case sg.WIN_CLOSED:
